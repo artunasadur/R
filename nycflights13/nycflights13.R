de54a6193133ac5d323b4e6a0bcb1d3b32efa973
artunasadur <artunasadur@gmail.com>
2019-02-13 12:23:15 -0500

    Update nycflights13.R

--- nycflights13/nycflights13.R
+++ nycflights13/nycflights13.R
@@ -1,7 +1,6 @@
 library(nycflights13)
 library(tidyverse)
-dp <- flights
-
+data("flights")
 flights %>% glimpse()
 flights <- flights %>% drop_na()
 
@@ -14,9 +13,9 @@ flights %>% filter(origin == "EWR", dest == "ATL") %>% summarise(mean = mean(arr
 (flights %>% filter(origin == "EWR") %>% group_by(carrier) %>%  summarise(avg_dep_delay = mean(dep_delay)) %>% arrange(avg_dep_delay))[1:3,]
 
 #Q3
-flights %>% filter(origin == "EWR") %>% group_by(hour) %>% summarise(avg_dep_delay = mean(dep_delay))
+(flights %>% filter(origin == "EWR") %>% group_by(hour) %>% summarise(avg_dep_delay = mean(dep_delay)) %>% arrange((avg_dep_delay)))
 
 #Q4
-data_mat <- (flights %>% filter(origin == "JFK")%>% count(month))
-data_mat[data_mat[,2]==max(data_mat[,2]),] [1]
-
+# data_mat <- (flights %>% filter(origin == "JFK")%>% count(month))
+# data_mat[data_mat[,2]==max(data_mat[,2]),] [1]
+((flights %>% filter(origin == "JFK")%>% count(month)) %>% filter(n==max(n)))
